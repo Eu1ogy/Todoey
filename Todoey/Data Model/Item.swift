7a3195d1ee710b24bc88f18106892a1b83069418
Rian <zedcustom@gmail.com>
2018-03-06 13:19:49 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rian Loubser on 2018/03/06.
+//  Copyright © 2018 Rian Loubser. All rights reserved.
+//
+
+import Foundation
+
+class  Item {
+    var title: String = ""
+    var done: Bool = false
+}
